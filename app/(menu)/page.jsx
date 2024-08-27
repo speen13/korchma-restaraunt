af710dfd316629ee023f5b1ab6fa1966d153d220
Maksym Symonov <ssmaksss33@gmail.com>
2024-08-27 16:54:24 +0300

    two commit

--- app/(menu)/page.jsx
+++ app/(menu)/page.jsx
@@ -5,10 +5,15 @@
 import Autoplay from "embla-carousel-autoplay"
 import {Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious} from "@/components/ui/carousel";
 import {Card, CardContent} from "@/components/ui/card";
+import {useMediaQuery} from "@/app/hooks/use-media-query";
 
 
 export default function MainPage() {
+  const isDesktop = useMediaQuery('(min-width: 768px)')
+
   return (
+    <>
+
     <Carousel opts={{
       align:"start",
       loop: true,
@@ -34,5 +39,33 @@ export default function MainPage() {
       </CarouselContent>
 
     </Carousel>
+      {!isDesktop && (
+        <Carousel opts={{
+          align:"start",
+          loop: true,
+        }} plugins={[
+          Autoplay({
+            delay: 5000,
+            stopOnInteraction: false,
+            stopOnMouseEnter:true,
+          }),
+        ]} className="   max-w-screen-md">
+          <CarouselContent className=''>
+            {Array.from({ length: 5 }).map((_, index) => (
+              <CarouselItem key={index}>
+                <div className="p-1">
+                  <Card className=''>
+                    <CardContent className='flex aspect-square items-center justify-center p-6'>
+                      <span className="text-4xl font-semibold">{index + 1}</span>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+
+        </Carousel>
+      )}
+    </>
   )
 }
